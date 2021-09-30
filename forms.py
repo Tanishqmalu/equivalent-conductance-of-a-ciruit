6c1c3dc1dcf07a9697089a8b1838fb3602b186de
Tarusi Mittal <54908632+Tarusimittal@users.noreply.github.com>
2021-10-01 00:06:43 +0530

    Tanishq1
    Changes done

--- forms.py
+++ forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 class inputform(FlaskForm):
         Input_circuit= StringField('Circuit String', validators=[DataRequired(), Length(min=3, max=50)])
-        submit = SubmitField('Find the answer')
-        Answer= StringField('Final Answer')
+        submit = SubmitField('Find the answer here')
+        Answer= StringField('Final Answer is')
